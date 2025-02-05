35c62cfca6defe05c0dd46ad8866788e00b84430
samrat-rock <samratpandey715@gmail.com>
2025-02-05 18:13:38 +0545

    updated one

--- src/app/Components/Hero.js
+++ src/app/Components/Hero.js
@@ -28,9 +28,8 @@ function hero() {
         </div>
 
         <h1 className='font-semibold text-[52px] text-white ss:text-[68px] ss:leading-[100px] leading-[75px] w-full'>
-          Payment Method
-
-        </h1>
+  Payment Method
+</h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.</p>
