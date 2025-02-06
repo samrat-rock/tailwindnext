bb43815d415e29315de1eca6cb17da93775c2ab8
samrat-rock <samratpandey715@gmail.com>
2025-02-06 12:18:46 +0545

    finished working on tailwind project for pratices

--- src/app/Components/Clients.js
+++ src/app/Components/Clients.js
@@ -1,11 +1,22 @@
+import { clients } from '../contants/NavLinks'
+import styles from '../style'
 import React from 'react'
+import Image from 'next/image'
 
-function clients() {
+function client() {
   return (
-    <div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client)=>(
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+            <Image src={client.logo} alt='client' className='sm:w-[192px] w-[100px] object-contain'/>
+
+          </div>
+        ))}
+      </div>
       
-    </div>
+    </section>
   )
 }
 
-export default clients
+export default client
